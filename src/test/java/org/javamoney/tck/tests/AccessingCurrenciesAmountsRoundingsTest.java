69a663d80405e271e024fc3ab8ff01905114a7d3
Anatole Tresch <atsticks@gmail.com>
2014-03-20 01:03:28 +0100

    Added TCK Test skeletons.

--- src/test/java/org/javamoney/tck/tests/AccessingCurrenciesAmountsRoundingsTest.java
+++ src/test/java/org/javamoney/tck/tests/AccessingCurrenciesAmountsRoundingsTest.java
@@ -11,6 +11,7 @@
 
 package org.javamoney.tck.tests;
 
+import org.jboss.test.audit.annotations.SpecAssertion;
 import org.jboss.test.audit.annotations.SpecVersion;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,101 +22,146 @@ import org.junit.Test;
 @SpecVersion(spec = "JSR 354", version = "1.0.0")
 public class AccessingCurrenciesAmountsRoundingsTest{
 
-    @Test
-    public void placeholder(){
+    // ****************** A. Accessing Currencies *******************
+
+
+    /**
+     * Test if MonetaryCurrencies provides all ISO related entries,
+     similar to the JDK.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-A1")
+    public void testAllISOCurrenciesAvailable(){
         Assert.fail();
     }
 
-    /*
-    <group>
-			<text>A. Accessing Currencies</text>
-			<assertion id="427-A1">
-				<text>Test if MonetaryCurrencies provides all ISO related entries,
-					similar to the JDK.
-				</text>
-			</assertion>
-			<assertion id="427-A2">
-				<text>Test if MonetaryCurrencies provides all Locale related
-					entries, similar to the JDK.
-				</text>
-			</assertion>
-			<assertion id="427-A3">
-				<text>Test if MonetaryCurrencies provides correct check for ISO
-					codes.
-				</text>
-			</assertion>
-			<assertion id="427-A4">
-				<text>Test if MonetaryCurrencies provides correct check for
-					Locales.
-				</text>
-			</assertion>
-			<assertion id="427-A5">
-				<text>Test for custom MonetaryCurrencies provided, based on the TCK
-					TestProvider.
-				</text>
-			</assertion>
-		</group>
-		<group>
-			<text>B. Accessing Monetary Amount Factories</text>
-			<assertion id="427-B1">
-				<text>Ensure the types available, must be at least one type (if one
-					has a specified AmountFlavor, 2 are recommended).
-				</text>
-			</assertion>
-			<assertion id="427-B2">
-				<text>Ensure amount factories are accessible for all types
-					available,
-					providing also the
-					some test implementations with the
-					TCK.
-				</text>
-			</assertion>
-			<assertion id="427-B3">
-				<text>Ensure amount factories are accessible for all types
-					available,
-					providing also the
-					some test implementations with the
-					TCK,
-					and that
-					every factory accessed
-					is a new instance.
-				</text>
-			</assertion>
-			<assertion id="427-B4">
-				<text>Ensure correct query function implementations, providing also
-					the
-					some test implementations with the TCK.
-				</text>
-			</assertion>
-			<assertion id="427-B5">
-				<text>Ensure a default factory is returned. Test javamoney.config
-					for
-					configuring default value.
-				</text>
-			</assertion>
-		</group>
-		<group>
-			<text>C. Accessing Roundings</text>
-			<assertion id="427-C1">
-				<text>Access roundings using all defined currencies, including TCK
-					custom currencies.
-				</text>
-			</assertion>
-			<assertion id="427-C2">
-				<text>Access roundings using a MonetaryContext. Use different
-					MathContext/RoundingMode, as an attribute, when running
-					on the JDK.
-				</text>
-			</assertion>
-			<assertion id="427-C3">
-				<text>Access custom roundings and ensure TCK custom roundings are
-					registered.
-				</text>
-			</assertion>
-			<assertion id="427-C4">
-				<text>Test TCK custom roundings.
-				</text>
-			</assertion>
-		</group>
+    /**
+     * Test if MonetaryCurrencies provides all Locale related
+     entries, similar to the JDK.
      */
+    @Test @SpecAssertion(section="4.2.7", id = "427-A2")
+    public void testAllLocaleCurrenciesAvailable(){
+        Assert.fail();
+    }
+
+    /**
+     * Test if MonetaryCurrencies provides  correct instance with ISO
+     codes.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-A3")
+    public void testCorrectISOCodes(){
+        Assert.fail();
+    }
+
+    /**
+     * Test if MonetaryCurrencies provides correct instance with
+     Locales.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-A4")
+    public void testCorrectLocales(){
+        Assert.fail();
+    }
+
+    /**
+     * Test for custom MonetaryCurrencies provided, based on the TCK
+     TestProvider.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-A5")
+    public void testCustomCurrencies(){
+        Assert.fail();
+    }
+
+    // ********************************* B. Accessing Monetary Amount Factories ***********************
+
+    /**
+     * Ensure the types available, must be at least one type (if one
+     has a specified AmountFlavor, 2 are recommended).
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-B1")
+    public void testAmountTypesDefined(){
+        Assert.fail();
+    }
+
+    /**
+     * Ensure amount factories are accessible for all types
+     available,
+     providing also the
+     some test implementations with the
+     TCK.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-B2")
+    public void testAmountTypesProvided(){
+        Assert.fail();
+    }
+
+    /**
+     * Ensure amount factories are accessible for all types
+     available,
+     providing also the
+     some test implementations with the
+     TCK,
+     and that
+     every factory accessed
+     is a new instance.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-B3")
+    public void testAmountTypesInstantiatable(){
+        Assert.fail();
+    }
+
+    /**
+     * Ensure correct query function implementations, providing also
+     the some test implementations with the TCK.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-B4")
+    public void testAmountQueryType(){
+        Assert.fail();
+    }
+
+    /**
+     * Ensure a default factory is returned. Test javamoney.config
+     for  configuring default value.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-B5")
+    public void testAmountDefaultType(){
+        Assert.fail();
+    }
+
+    // ********************************* C. Accessing Roundings *****************************
+
+    /**
+     * Access roundings using all defined currencies, including TCK
+     custom currencies.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-C1")
+    public void testAccessRoundingsForCustomCurrencies(){
+        Assert.fail();
+    }
+
+    /**
+     * Access roundings using a MonetaryContext. Use different
+     MathContext/RoundingMode, as an attribute, when running
+     on the JDK.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-C2")
+    public void testAccessRoundingsWithMonetaryContext(){
+        Assert.fail();
+    }
+
+    /**
+     * Access custom roundings and ensure TCK custom roundings are
+     registered.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-C3")
+    public void testAccessCustomRoundings(){
+        Assert.fail();
+    }
+
+    /**
+     * Test TCK custom roundings.
+     */
+    @Test @SpecAssertion(section="4.2.7", id = "427-C4")
+    public void testCustomRoundings(){
+        Assert.fail();
+    }
+
 }
